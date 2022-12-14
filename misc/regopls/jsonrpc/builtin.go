f872a15252082bcdcd9c14cf499f48f9b9f5b2e0
Alan Ma <alan_ma@live.ca>
2022-12-14 00:41:18 -0500

    Add skeleton language server from github.com/TobiasYin/go-lsp

---
+++ misc/regopls/jsonrpc/builtin.go
@@ -0,0 +1,28 @@
+package jsonrpc
+
+import "context"
+
+// $/cancelRequest
+type cancelParams struct {
+	ID interface{} `json:"id"`
+}
+
+func cancelRequest(ctx context.Context, req interface{}) (interface{}, error) {
+	params := req.(*cancelParams)
+	if params.ID == nil {
+		return nil, nil
+	}
+	session := getSession(ctx)
+	session.cancelJob(params.ID)
+	return nil, nil
+}
+
+func CancelRequest() MethodInfo {
+	return MethodInfo{
+		Name: "$/cancelRequest",
+		NewRequest: func() interface{} {
+			return &cancelParams{}
+		},
+		Handler: cancelRequest,
+	}
+}
